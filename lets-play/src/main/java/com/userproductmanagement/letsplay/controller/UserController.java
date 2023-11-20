d173992bb1f560a05630d6ec024ed87f11d4792a
totallytiia <tlaukkan@student.hive.fi>
2023-11-20 18:57:18 +0200

    admin stuff

--- lets-play/src/main/java/com/userproductmanagement/letsplay/controller/UserController.java
+++ lets-play/src/main/java/com/userproductmanagement/letsplay/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.userproductmanagement.letsplay.controller;
 
 import com.userproductmanagement.letsplay.model.User;
-import com.userproductmanagement.letsplay.repository.UserRepository;
 import com.userproductmanagement.letsplay.service.UserService;
-import com.userproductmanagement.letsplay.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
