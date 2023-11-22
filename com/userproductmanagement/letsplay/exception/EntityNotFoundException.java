17dfe859f3bc58507d3660b80db67fbb5d196274
Tiia Laukkanen <tiialaukkanen@icloud.com>
2023-11-22 18:47:27 +0200

    Delete com/userproductmanagement/letsplay directory

--- com/userproductmanagement/letsplay/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.userproductmanagement.letsplay.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
--- com/userproductmanagement/letsplay/model/Role.java
+++
@@ -1,9 +0,0 @@
-package com.userproductmanagement.letsplay.model;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
-}
-
-
-
